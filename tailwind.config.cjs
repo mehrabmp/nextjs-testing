d896686c1dff14d2b371b52f98e99dc6fe3effe8
MehrabMP <mehrabmpdev@gmail.com>
2023-03-15 00:03:18 +0330

    feat: setup tailwindcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)', ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+};
